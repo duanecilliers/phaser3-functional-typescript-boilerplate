266e6bcc29d600eca2bcf65d1637067caa3ee8ff
Duane Cilliers <hello@duane.co.za>
2020-04-13 00:56:37 +0200

    Initial commit :tada:
    Most of the code was taken from
    [Phaser3.Boilerplate](https://github.com/Sidaroth/Phaser3.Boilerplate)
    and converted to TS. This is still a WIP

---
+++ src/utils/resizeCanvas.ts
@@ -0,0 +1,19 @@
+import gameConfig from 'configs/gameConfig'
+
+const resizeCanvas = function resizeCanvasFunc() {
+  const canvas: HTMLCanvasElement = document.querySelector('canvas')
+  const windowWidth = window.innerWidth
+  const windowHeight = window.innerHeight
+  const windowRatio = windowWidth / windowHeight
+  const gameRatio = gameConfig.GAME.VIEWWIDTH / gameConfig.GAME.VIEWHEIGHT
+  console.log('canvas', canvas)
+  if (windowRatio < gameRatio) {
+    canvas.style.width = `${windowWidth}px`
+    canvas.style.height = `${windowWidth / gameRatio}px`
+  } else {
+    canvas.style.width = `${windowHeight * gameRatio}px`
+    canvas.style.height = `${windowHeight}px`
+  }
+}
+
+export default resizeCanvas
